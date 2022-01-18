d424f615ba319d5b023915f2f1196c971f4110fc
sakibcse570 <17201122@uap-bd.edu>
2022-01-18 15:40:24 +0600

    Product reviews,offers,search result routes added

--- index.js
+++ index.js
@@ -25,6 +25,42 @@ app.get('/products/:productId', async (req, res) => {
     }
 }); //productId is going to be dynamic
 
+//GET Product Reviews
+app.get('/products/:productId/reviews', async (req, res) => {
+    const { productId } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/product-reviews/${productId}`);
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+//GET Product Offers
+app.get('/products/:productId/offers', async (req, res) => {
+    const { productId } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/gp/offer-listing/${productId}`);
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+//GET Search Result
+app.get('/search/:searchQuery', async (req, res) => {
+    const { searchQuery } = req.params;
+
+    try {
+        const response = await request(`${baseUrl}&url=https://www.amazon.com/s?k=${searchQuery}`);
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
